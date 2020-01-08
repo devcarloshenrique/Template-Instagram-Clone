4637ba4161a37e82243442d73cc0ef3b9d30c58a
devcarloshenrique <devcarloshenrique@gmail.com>
2020-01-07 23:44:09 -0200

    feat: initial development framework set up

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,9 @@ const app = express();
 //app.get é uma Rota. toda função que tem req e res é um middle 
 //middle é um interceptador de chamada de requisições.
 app.get('/', (req, res) => {
-    return res.send('Hello Word')
-})
+	// Eviando uma resposta
+    return res.send(`Ola ${req.query.name}`);
+});
 
 
 // Porta, que o navegador vai acessar.
