275263fe0ead68e317d6c0a06d9f9e206f7df416
devcarloshenrique <devcarloshenrique@gmail.com>
2020-01-11 18:02:30 -0200

    refactor: pages configured to respond in real time

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import api from '../services/api';
+import io from 'socket.io-client';
 
 import './Feed.css';
 
@@ -8,64 +10,80 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component {
-	// Quando á class é criada deste modo é obrigatorio retornar o metodo render()
+// state criado para armazenar informações dentro do componente 
+	state  = {
+		feed: [],
+	};
+
+// Quando um componente é escrito em formato de class, todo componente nesse formato tema acesso a este metodo
+	async componentDidMount() {
+		this.registerToSocket();
+
+		const response = await api.get('posts');
+
+		this.setState({ feed: response.data })
+	}
+
+	registerToSocket = () => {
+		const socket = io('http://localhost:80');
+
+		socket.on('post', newPost => {
+			this.setState({ feed: [newPost, ...this.state.feed]})
+		})
+
+		socket.on('like', likedPost => {
+			this.setState({
+			 feed: this.state.feed.map(post => 
+			 	post._id === likedPost._id ? likedPost : post
+			 )
+			});
+		})
+	}
+
+	handleLike = id => {
+		api.post(`/posts/${id}/like`); 
+	}
+
+// Quando á class é criada deste modo é obrigatorio retornar o metodo render()
 	render() {
 		return (
 			<section id="post-list">
-				<article>
+
+			{ this.state.feed.map(post => (
+						
+				<article key={post._id}>
 					<header>
 						<div className="user-info">
-							<span>devcarloshenrique</span>
-							<span className="place">Diretamente do nordeste para o mundo</span>
+							<span>{post.author}</span>
+							<span className="place">{post.place}</span>
 						</div>
 
 						<img src={more} alt="Mais"/>
 					</header>
 
-						<img src="http://localhost:80/files/beat.jpg" alt="" />
+						<img src={`http://localhost:80/files/${post.image}`} alt="" />
 
 						<footer>
-							<div className="actions">
-								<img src={like} />
+							<div className="actions">.
+								<button type="button" onClick={ () => this.handleLike(post._id)}>
+									<img src={like} />
+								</button>
 								<img src={comment} />
 								<img src={send} />
 							</div>
 
-							<strong>900 curtidas </strong>
+							<strong>{post.likes} curtidas </strong>
 
-							<p>InstaClone já é uma realidade.
-								<span>#clone #insta #InstaClone #top</span>
+							<p>{post.description}
+								<span>{post.hashtags}</span>
 							</p>
 						</footer>
 				</article>
 
-				<article>
-					<header>
-						<div className="user-info">
-							<span>devcarloshenrique</span>
-							<span className="place">Diretamente do nordeste para o mundo</span>
-						</div>
-
-						<img src={more} alt="Mais"/>
-					</header>
-
-						<img src="http://localhost:80/files/beat.jpg" alt="" />
-
-						<footer>
-							<div className="actions">
-								<img src={like} />
-								<img src={comment} />
-								<img src={send} />
-							</div>
-
-							<strong>900 curtidas </strong>
-
-							<p>InstaClone já é uma realidade.
-								<span>#clone #insta #InstaClone #top</span>
-							</p>
-						</footer>
-				</article>
+				))}
+			
 			</section>
+
 		)
 	}
 }
